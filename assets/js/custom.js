ffa90af7319558015e9b19783033700898d7bcf9
Lucas_Souza <lukfdg@gmail.com>
2020-04-15 18:02:53 +0000

    ‘commit’

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	const headerSize = document.getElementById('header').clientHeight;
-	document.getElementById('container-background').style.height = (window.innerHeight - headerSize) + "px";
+document.getElementById('container-background').style.height = (window.innerHeight - headerSize) + "px";
 });
+
